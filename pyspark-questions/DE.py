0008788101ee970de79cb93c7ccc144d577a8585
sharayulone <lone.s.sharayu@gmail.com>
2026-02-28 20:34:34 +0530

    Enhance DE.py with optimizer explanations and CSV read example
    Added explanations for Catalyst and Tungsten optimizers, and provided an example of reading a CSV file in PySpark.

--- pyspark-questions/DE.py
+++ pyspark-questions/DE.py
@@ -1,3 +1,16 @@
 ## What is the difference between RDD, DataFrame?
 ##-> RDD are low level api and it does not contain schema
 ## -> dataframes are distributed with schema (table)
+
+## What is catalyst optimizer?
+## -> Catalyst Optimizer is Spark SQL’s query optimization engine that converts logical plans into 
+##  optimized physical execution plans using rule-based and cost-based optimizations.
+
+## what is tungsten optimizer?
+## -> Tungsten is Spark’s execution engine that improves performance by managing memory efficiently, processing data in 
+## binary format, and using whole-stage code generation to reduce CPU and garbage collection overhead.
+
+## How do you read a CSV file in PySpark?
+df = spark.read.option("header", True)\
+          .option("inferSchema", True)\
+          .csv("file_path")
